6f1ca423150e69e064705dbab489c25810e904f6
Eduardo Shanahan <contact@eduardoshanahan.com>
2017-12-15 15:16:43 +0000

    Updated url in script to generate tags in Docker Hub

--- build_image.sh
+++ build_image.sh
@@ -1,2 +1,2 @@
 IMAGE_VERSION=$(cat version.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
-curl -H "Content-Type: application/json" --data '{"source_type": "Tag", "source_name":"'$IMAGE_VERSION'"}' -X POST https://registry.hub.docker.com/u/eduardoshanahan/fabric/trigger/29a6a74c-b879-4d98-840c-575c1e561688/
+curl -H "Content-Type: application/json" --data '{"source_type": "Tag", "source_name":"'$IMAGE_VERSION'"}' -X POST https://registry.hub.docker.com/u/eduardoshanahan/python/trigger/b7d46bb5-f9ba-4ef7-b595-4041b4e0d99f/
