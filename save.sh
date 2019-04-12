793d9f07db894d93e0a050ea5f6dfa796e23fbc1
Eduardo Shanahan <contact@eduardoshanahan.com>
2019-04-12 14:44:00 +0100

    Fixing tagging scripts

--- save.sh
+++ save.sh
@@ -1,15 +1,21 @@
 IMAGE_VERSION=$(cat version.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
+
 echo 'Pushing to bitbucket'
 git push bitbucket
 echo 'Pushing to github'
 git push github
+
 echo 'Checking tag'
-if [ ! $(git tag -l $IMAGE_VERSION) ]; then
-    echo 'Creating new tag'
-    git tag $IMAGE_VERSION
-    echo 'Pushing tags to bitbucket'
-    git push bitbucket --tags
-    echo 'Pushing tags to github'
-    git push github --tags
+if [ $(git tag -l $IMAGE_VERSION) ]; then
+    echo 'Removing previously registered tag'
+    git tag -d $IMAGE_VERSION
+else
+    echo 'Tag was not created before'
 fi
-    echo 'Tag already existing'
+echo 'Creating new tag '$IMAGE_VERSION
+git tag $IMAGE_VERSION
+
+echo 'Pushing tags to bitbucket'
+git push bitbucket --tags
+echo 'Pushing tags to github'
+git push github --tags
